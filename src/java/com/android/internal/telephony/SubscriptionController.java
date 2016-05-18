37774580f13125ac2d3154c6539cf2557d3d6282
Roman Birg <roman@cyngn.com>
2016-05-17 17:14:44 -0700

    SubscriptionInfoUpdater: improved sim swap
    We now keep track of the last number of inserted SIMs updated for. So
    when the number of SIMs has changed we do the following:
    
     1 sim: Clear stale defaults
            Disable SMS prompt, SMS app will default to current sim
            Disable data if the SIM left in the tray is not the one chosen
    
     m sim: Enable SMS prompt if we don't have a valid default (aka always
            ask)
    
    Furthermore, when we detect the device is an MSIM device and there is
    only one active sub, return the active sub to the UI layers (so SIM
    selection screen displays proper, grayed out, current settings).
    
    Ticket: CYNGNOS-2185, CYNGNOS-2570
    
    Change-Id: Ib1b4f4d87640e4b20722295304c58dff7dbeecbb
    Signed-off-by: Roman Birg <roman@cyngn.com>

--- src/java/com/android/internal/telephony/SubscriptionController.java
+++ src/java/com/android/internal/telephony/SubscriptionController.java
@@ -188,6 +188,12 @@ public class SubscriptionController extends ISub.Stub {
         return subUpdater != null && sSlotIdxToSubId.size() == subUpdater.getInsertedSimCount();
     }
 
+    private boolean isSingleSimActiveOnMSIM() {
+        final SubscriptionInfoUpdater subUpdater = PhoneFactory.getSubscriptionInfoUpdater();
+        return subUpdater != null && subUpdater.getInsertedSimCount() == 1
+                && getActiveSubInfoCountMax() > 1;
+    }
+
     private SubscriptionController(Phone phone) {
         mContext = phone.getContext();
         mCM = CallManager.getInstance();
@@ -1363,6 +1369,10 @@ public class SubscriptionController extends ISub.Stub {
 
     @Override
     public int getDefaultSmsSubId() {
+        if (isSingleSimActiveOnMSIM()) {
+            if (VDBG) logd("[getDefaultSmsSubId] overridden to current single active sim");
+            return mDefaultFallbackSubId;
+        }
         int subId = Settings.Global.getInt(mContext.getContentResolver(),
                 Settings.Global.MULTI_SIM_SMS_SUBSCRIPTION,
                 SubscriptionManager.INVALID_SUBSCRIPTION_ID);
@@ -1394,6 +1404,10 @@ public class SubscriptionController extends ISub.Stub {
 
     @Override
     public int getDefaultVoiceSubId() {
+        if (isSingleSimActiveOnMSIM()) {
+            if (VDBG) logd("[getDefaultVoiceSubId] overridden to current single active sim");
+            return mDefaultFallbackSubId;
+        }
         int subId = Settings.Global.getInt(mContext.getContentResolver(),
                 Settings.Global.MULTI_SIM_VOICE_CALL_SUBSCRIPTION,
                 SubscriptionManager.INVALID_SUBSCRIPTION_ID);
@@ -1403,6 +1417,10 @@ public class SubscriptionController extends ISub.Stub {
 
     @Override
     public int getDefaultDataSubId() {
+        if (isSingleSimActiveOnMSIM()) {
+            if (VDBG) logd("[getDefaultDataSubId] overridden to current single active sim");
+            return mDefaultFallbackSubId;
+        }
         int subId = Settings.Global.getInt(mContext.getContentResolver(),
                 Settings.Global.MULTI_SIM_DATA_CALL_SUBSCRIPTION,
                 SubscriptionManager.INVALID_SUBSCRIPTION_ID);
@@ -1455,8 +1473,10 @@ public class SubscriptionController extends ISub.Stub {
         // FIXME is this still needed?
         updateAllDataConnectionTrackers();
 
-        Settings.Global.putInt(mContext.getContentResolver(),
-                Settings.Global.MULTI_SIM_DATA_CALL_SUBSCRIPTION, subId);
+        if (!isSingleSimActiveOnMSIM()) {
+            Settings.Global.putInt(mContext.getContentResolver(),
+                    Settings.Global.MULTI_SIM_DATA_CALL_SUBSCRIPTION, subId);
+        }
         broadcastDefaultDataSubIdChanged(subId);
     }
 
@@ -1491,7 +1511,7 @@ public class SubscriptionController extends ISub.Stub {
         if (SubscriptionManager.isValidSubscriptionId(subId)) {
             int phoneId = getPhoneId(subId);
             if (phoneId >= 0 && (phoneId < mTelephonyManager.getPhoneCount()
-                    || mTelephonyManager.getSimCount() == 1)) {
+                    || isSingleSimActiveOnMSIM())) {
                 if (DBG) logdl("[setDefaultFallbackSubId] set mDefaultFallbackSubId=" + subId);
                 mDefaultFallbackSubId = subId;
                 // Update MCC MNC device configuration information
